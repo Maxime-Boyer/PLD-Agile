b45d9ca8b625c4dd5c5de52b2d8d02c21b3f4d60
A L <amine.lbath@insa-lyon.fr>
2021-10-09 13:24:43 +0200

    Updated the HelloWorld class and added a package

--- src/example/HelloWorld.java
+++ src/example/HelloWorld.java
@@ -1,8 +1,11 @@
+package example;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+        System.out.println("Test Amine");
     }
 
 }
