6af9466154b51ac5d17895819433cb6c53fc0ab9
Brahim-Mahmoudi <brahim.mahmoudi@insa-lyon.fr>
2021-10-11 17:38:07 +0200

    Changement brahim

---
+++ src/RACOURCIF/Adresse.java
@@ -0,0 +1,16 @@
+package RACOURCIF;
+
+import java.util.List;
+
+public class Adresse {
+    private float latitude;
+    private float longitude;
+    private List<Segment> segmentSortant;
+
+
+    public Adresse(float latitude, float longitude, List<Segment> segmentSortant) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.segmentSortant = segmentSortant;
+    }
+}
