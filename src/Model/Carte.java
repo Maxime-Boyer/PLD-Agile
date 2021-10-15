ae064aaa422b9933f45e9bb6752c9e5eea2f415b
eydakhil <el-yazid.dakhil@insa-lyon.fr>
2021-10-15 10:03:41 +0200

    Lecteur Carte XML

--- src/Model/Carte.java
+++ src/Model/Carte.java
@@ -8,5 +8,12 @@ public class Carte {
     private Map <String,Adresse>listeSegments;
     private String nomCarte;
 
+    public Carte(String nomCarte) {
+        this.nomCarte = nomCarte;
+    }
 
+    public Adresse obtenirAdresseParId(String id){
+        return listeAdresses.get(id);
+
+    }
 }
--- src/Model/LecteurXML.java
+++ src/Model/LecteurXML.java
@@ -1,8 +1,50 @@
 package Model;
 
+import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 public class LecteurXML {
 
     public LecteurXML() {
     }
 
+    /**
+     *
+     * @param nomFichier
+     * @throws ParserConfigurationException
+     * @throws SAXException
+     */
+    public void lectureCarte(String nomFichier) throws ParserConfigurationException, SAXException {
+        try{
+            File file = new File("smallMap.xml");
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document document =  db.parse(file);
+            document.getDocumentElement().normalize();
+            NodeList nList = document.getElementsByTagName("intersection");
+
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    Float latitude = Float.parseFloat(eElement.getAttribute("latitude"));
+                    Float longitude = Float.parseFloat(eElement.getAttribute("longitude"));
+                    Integer id = Integer.parseInt(eElement.getAttribute("id"));
+
+                    Adresse adresse = new Adresse(latitude,longitude,id);
+                }
+            }
+        }
+        catch(IOException e){
+
+        }
+    }
 }
