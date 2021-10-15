8cca34bb65cbe1e3778fa05a8354a6d97bda9dd8
Maxime Boyer <75278784+Max13crypt@users.noreply.github.com>
2021-10-15 11:07:42 +0200

    Yazid

--- src/Model/Segment.java
+++ src/Model/Segment.java
@@ -5,29 +5,23 @@ public class Segment {
     private Adresse origine;
     private Adresse destination;
     private String nom;
-    private Long longueur;
+    private Double longueur;
 
 
-    public Segment(Adresse origine, Adresse destination, String nom, Long longueur) {
+    public Segment(Adresse origine, Adresse destination, String nom, Double longueur) {
         this.origine = origine;
         this.destination = destination;
         this.nom = nom;
         this.longueur = longueur;
     }
 
-    public Adresse getOrigine() {
-        return origine;
-    }
-
-    public Adresse getDestination() {
-        return destination;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public Long getLongueur() {
-        return longueur;
+    @Override
+    public String toString() {
+        return "Segment{" +
+                "origine=" + origine +
+                ", destination=" + destination +
+                ", nom='" + nom + '\'' +
+                ", longueur=" + longueur +
+                '}';
     }
 }
