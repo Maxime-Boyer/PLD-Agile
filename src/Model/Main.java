812f348e8b0903efd051959164e19f09674d28a6
Brahim-Mahmoudi <brahim.mahmoudi@insa-lyon.fr>
2021-10-15 19:06:47 +0200

    test parsing dans le main

--- src/Model/Main.java
+++ src/Model/Main.java
@@ -5,10 +5,11 @@ import org.xml.sax.SAXException;
 import javax.swing.*;
 import javax.xml.parsers.ParserConfigurationException;
 import java.awt.*;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws ParserConfigurationException, SAXException {
-        //Carte
+        /*//Carte
         JFrame yourJFrame = new JFrame();
         FileDialog fd = new FileDialog(yourJFrame, "Choose a file", FileDialog.LOAD);
         fd.setDirectory("C:\\");
@@ -21,7 +22,15 @@ public class Main {
             System.out.println("You chose " + filename);
 
         LecteurXML lecteur = new LecteurXML();
-        lecteur.lectureCarte(filename);
+        lecteur.lectureCarte(filename);*/
 
+        LecteurXML lecteur = new LecteurXML();
+        lecteur.lectureCarte("/Users/bramss/Documents/4_IF/4IF_MOI/S1/AGILE/PLD-Agile/src/FichiersXML/largeMap.xml");
+
+        List<Requete> requetes = lecteur.lectureRequete("/Users/bramss/Documents/4_IF/4IF_MOI/S1/AGILE/PLD-Agile/src/FichiersXML/requestsLarge9.xml");
+
+        for ( Requete r : requetes){
+            System.out.println(r);
+        }
     }
 }
