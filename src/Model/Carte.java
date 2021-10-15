3bae6dfec1df8efff33ebcc4ed0be641c369bfc6
Maxime Boyer <75278784+Max13crypt@users.noreply.github.com>
2021-10-15 09:33:21 +0200

    Update modèle du domaine 15/10

--- src/Model/Carte.java
+++ src/Model/Carte.java
@@ -5,9 +5,12 @@ import java.util.List;
 public class Carte {
     private List<Adresse> listeAdresses;
     private List<Segment> listeSegments;
+    private String nomCarte;
 
     public Carte(List<Adresse> listeAdresses, List<Segment> listeSegments) {
         this.listeAdresses = listeAdresses;
         this.listeSegments = listeSegments;
     }
+
+
 }
