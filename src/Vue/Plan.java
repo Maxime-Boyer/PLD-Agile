6a914073e568d7693f573fb6039e188cca1771f1
Maxime Boyer <75278784+Max13crypt@users.noreply.github.com>
2021-10-17 15:48:12 +0200

    Modify Plan

--- src/Vue/Plan.java
+++ src/Vue/Plan.java
@@ -8,6 +8,8 @@ import org.xml.sax.SAXException;
 import javax.swing.*;
 import javax.xml.parsers.ParserConfigurationException;
 import java.awt.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Map;
 
 public class Plan extends JPanel {
@@ -64,6 +66,8 @@ public class Plan extends JPanel {
     public void paintComponent(Graphics g)
     { super.paintComponent(g);
 
+        //System.out.println("hauteur ecran : " + hauteurEcran + " largeur ecran : " + largeurEcran);
+
         Graphics2D g2 = (Graphics2D) g;
 
         g2.setColor(Color.BLACK);
@@ -71,11 +75,20 @@ public class Plan extends JPanel {
 
         // BackGround
 
-        g2.setColor(Color.WHITE);
+        g2.setColor(Color.RED);
 
         g2.fillRect(0, 0, getSize().width, getSize().height);
 
-        g2.setColor(Color.BLACK);
+        g2.setColor(Color.WHITE);
+
+        g.drawLine(0, 56, 350, 267);
+        g.drawLine(56, 5, 40, 27);
+        g.drawLine(189, 39, 50, 2);
+
+        System.out.println("maxLongitudeCarte" + maxLongitudeCarte);
+        System.out.println("maxLatitudeCarte" + maxLatitudeCarte);
+        System.out.println("minLatitudeCarte" + minLatitudeCarte);
+        System.out.println("minLongitudeCarte" + minLongitudeCarte);
 
         for (int i = 0; i < carte.getListeSegments().size(); i++) {
             Adresse origine = carte.getListeSegments().get(i).getOrigine();
@@ -84,29 +97,48 @@ public class Plan extends JPanel {
             int origineY = valeurY(origine.getLatitude());
             int destinationX = valeurX(destination.getLongitude());
             int destinationY = valeurY(destination.getLatitude());
+            System.out.println("x1 : " + origineX + " y1 : " + origineY + " x2 : " + destinationX + " y2 : " + destinationY);
 
             g.drawLine(origineX, origineY, destinationX, destinationY);
-            System.out.println("Segment " + i);
+            //System.out.println("Segment " + i);
         }
         //g.drawString("HELLO JAVA");
     }
 
     public int valeurX(float longitude){
 
-        float ecartLongitude = maxLongitudeCarte - minLongitudeCarte;
-        float coeffX = ecartLongitude / largeurEcran;
+        System.out.println("longitude " + longitude);
+        //float ecartLongitude = maxLongitudeCarte - minLongitudeCarte;
+        float ecartLongitude = 0.022F;
+        float coeffX = largeurEcran / ecartLongitude;
         int valeurXPixel = (int) Math.ceil((maxLongitudeCarte - longitude)*coeffX);
-
+        System.out.println("coeff X : " + coeffX);
+        System.out.println("ecartLongitude : " + ecartLongitude);
         return valeurXPixel;
     }
 
     public int valeurY(float latitude){
+        System.out.println("latitude " + latitude);
+        //float ecartLatitude = maxLatitudeCarte - minLatitudeCarte;
 
-        float ecartLatitude = maxLatitudeCarte - minLatitudeCarte;
-        float coeffY = ecartLatitude / hauteurEcran;
-        int valeurYPixel = (int) Math.ceil((maxLongitudeCarte - latitude)*coeffY);
-
+        float ecartLatitude = 0.022F;
+        float coeffY = hauteurEcran / ecartLatitude;
+        int valeurYPixel = (int) Math.ceil((maxLatitudeCarte - latitude)*coeffY);
+        System.out.println("coeff Y : " + coeffY);
+        System.out.println("ecartLatitude : " + ecartLatitude);
         return valeurYPixel;
+    }
+
+    public int valeurXbis(float longitude){
+        float valeurX = (longitude+180)*(largeurEcran/360);
+        return (int)Math.ceil(valeurX);
+    }
+
+    public int valeurYbis(float latitude){
+        float latRad = (float) (latitude*Math.PI/180);
+        float mercN = (float) Math.log(Math.tan((Math.PI/4)+(latRad/2)));
+        float valeurY = (float) ((hauteurEcran/2)-(largeurEcran*mercN/(2*Math.PI)));
+        return (int)Math.ceil(valeurY);
     }
 
     public void maxLongitudeLatitudeCarte(){
