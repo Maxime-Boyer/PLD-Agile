7079915d9e9ca9bdddee39c8a9abb1f95a2638d2
Maxime Boyer <75278784+Max13crypt@users.noreply.github.com>
2021-10-19 10:36:15 +0200

    Changes by mac

--- src/Vue/BoutonRond.java
+++ src/Vue/BoutonRond.java
@@ -13,12 +13,20 @@ public class BoutonRond extends JButton{
 
     @Override
     public void paintComponent(Graphics g){
-        Graphics2D g2 = (Graphics2D) g;
+
+
+        Graphics2D g2 = (Graphics2D) g.create();
 
         g2.setColor(Color.PINK);
-        g2.setStroke(new BasicStroke(1));
-
-        g2.fillRoundRect(x,y,width, height, rayonCourbure, rayonCourbure);
+        System.out.println("paint bouton rond");
+        System.out.println("getX()" + getX());
+        System.out.println("getY()" + getY());
+        System.out.println("getWidth()" + getWidth());
+        System.out.println("getHeight()" + getHeight());
+        g2.fillRoundRect(getX(),getY(),getWidth(), getHeight(), getWidth(), getHeight());
+        //
+        //super.paintComponent(g);
+        //this.setVisible(true);
     }
 
 }
