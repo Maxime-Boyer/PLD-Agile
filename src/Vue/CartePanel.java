7692964a60d86788123b4e7f443fe30ec6f60243
quentin-marc <quentinmarc81@gmail.com>
2021-10-28 08:05:02 +0200

    couleurs affichage textuel requetes

--- src/Vue/CartePanel.java
+++ src/Vue/CartePanel.java
@@ -181,18 +181,10 @@ public class CartePanel extends JPanel {
             int valeurXDepot = valeurX(lonDepot);
             int valeurYDepot = valeurY(latDepot);
 
-            Random rand = new Random();
-            int maximumCouleur = 255;
-            int r = rand.nextInt(maximumCouleur);
-            int gr = rand.nextInt(maximumCouleur);
-            int b = rand.nextInt(maximumCouleur);
-
-            g2.setColor(new Color(r, gr, b));
+            g2.setColor(tournee.getListeRequetes().get(i).getCouleur());
 
             g2.fillRoundRect(valeurXCollecte - 7, valeurYCollecte - 7, 14, 14, 14, 14);
             g2.fillRect(valeurXDepot - 7, valeurYDepot - 7, 14, 14);
-
-
         }
     }
 
