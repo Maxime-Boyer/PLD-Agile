ec8324ea32a5d7f5937f9f53fbbe94fa6b79abd4
Brahim-Mahmoudi <brahim.mahmoudi@insa-lyon.fr>
2021-11-10 18:59:12 +0100

    javadoc observeurs

--- src/Observer/Observable.java
+++ src/Observer/Observable.java
@@ -7,16 +7,32 @@ import java.util.Collection;
 
 public class Observable {
     private Collection<Observer> obs;
+
+    /**
+     * Constructeur d'observable
+     */
     public Observable(){
         obs = new ArrayList<Observer>();
     }
+
+    /**
+     * methode qui ajoute un observeur à la collection d'observeurs
+     * @param o
+     */
     public void addObserver(Observer o){
         if (!obs.contains(o)) obs.add(o);
     }
+    /**
+     * methode qui notifie les observeurs
+     */
     public void notifyObservers(Object arg){
         for (Observer o : obs)
             o.update(this, arg);
     }
+
+    /**
+     * methode qui notifie les observeurs
+     */
     public void notifyObservers(){
         notifyObservers(null);
     }
