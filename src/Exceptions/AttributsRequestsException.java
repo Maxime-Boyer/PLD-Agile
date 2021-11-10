5eda99186a89858aa3e9db7a8bd12fed8b41dca0
Brahim-Mahmoudi <brahim.mahmoudi@insa-lyon.fr>
2021-11-10 16:56:18 +0100

    javadoc exception

--- src/Exceptions/AttributsRequestsException.java
+++ src/Exceptions/AttributsRequestsException.java
@@ -1,7 +1,10 @@
 package Exceptions;
 
 public class AttributsRequestsException extends Exception{
-
+    /**
+     * Exception lancée lorsque la balise request à un soucis dans le nombre d'attribut ou dans le contenu des attributs
+     * @param message: message emis lors de la découverte de l'exception
+     */
     public AttributsRequestsException(String message) {
         super(message);
         System.out.println(message);
--- src/Exceptions/IncompatibleLongitudeException.java
+++ src/Exceptions/IncompatibleLongitudeException.java
@@ -1,7 +1,10 @@
 package Exceptions;
 
 public class IncompatibleLongitudeException extends Exception{
-
+    /**
+     * Exception lancée lorsque une longitude n'a pas un format adéquat
+     * @param message: message emis lors de la découverte de l'exception
+     */
     public IncompatibleLongitudeException(String message) {
         super(message);
         System.out.println(message);
