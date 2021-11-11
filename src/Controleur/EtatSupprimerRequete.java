edd553db593042842b90114f4e3518ff20630f2f
Maxime Boyer <75278784+Max13crypt@users.noreply.github.com>
2021-11-11 16:05:10 +0100

    Changes by mac

--- src/Controleur/EtatSupprimerRequete.java
+++ src/Controleur/EtatSupprimerRequete.java
@@ -12,6 +12,7 @@ public class EtatSupprimerRequete implements Etat {
     @Override
     public void supressionRequete(Controleur controleur, Fenetre fenetre, ListeDeCommandes listeDeCommandes, Tournee tournee, Carte carte, Requete requete){
         try{
+            listeDeCommandes.reinitialiser();
             System.out.println(requete);
             Etape etapePrecedentCollecte = tournee.precedentCollecte(requete);
             Etape etapePrecedentDepot = tournee.precedentDepot(requete);
